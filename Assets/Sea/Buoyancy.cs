8595753d0731fdab3d339532cfcf3a92e5eb465a
Memnun <zeronocool@gmail.com>
2020-11-13 16:42:37 -0500

    fixed root levels

---
+++ Assets/Sea/Buoyancy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buoyancy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //base buoyant force
+        if (transform.position.y < 0)
+        {
+            transform.GetComponent<Rigidbody>().AddForce(0, 10*transform.position.y*transform.position.y, 0);
+        }
+    }
+}
