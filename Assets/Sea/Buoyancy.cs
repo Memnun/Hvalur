69b86e6b8ae482bd5237e9de3f6241b651cb0eac
Memnun <zeronocool@gmail.com>
2020-11-14 05:57:40 -0500

    fixed buoyancy
    needed drag, idiot

--- Assets/Sea/Buoyancy.cs
+++ Assets/Sea/Buoyancy.cs
@@ -53,36 +53,36 @@ public class Buoyancy : MonoBehaviour
         //righting buoyancy
         /*if (transform.TransformPoint(M1).y < 0)
         {
-            body.AddForceAtPosition(new Vector3(0,(d1*d1*(d3+d4)*mass),0), transform.TransformPoint(M1));
+            body.AddForceAtPosition(new Vector3(0,(d1*d1*(d3+d4)*transform.TransformPoint(M1).y*mass*drag),0), transform.TransformPoint(M1));
         }
         if (transform.TransformPoint(M2).y < 0)
         {
-            body.AddForceAtPosition(new Vector3(0,(d2*d2*(d3+d4)*mass),0), transform.TransformPoint(M2));
+            body.AddForceAtPosition(new Vector3(0,(d2*d2*(d3+d4)*transform.TransformPoint(M2).y*mass*drag),0), transform.TransformPoint(M2));
         }
         if (transform.TransformPoint(M3).y < 0)
         {
-            body.AddForceAtPosition(new Vector3(0,(d3*d3*(d1+d2)*mass),0), transform.TransformPoint(M3));
+            body.AddForceAtPosition(new Vector3(0,(d3*d3*(d1+d2)*transform.TransformPoint(M3).y*mass*drag),0), transform.TransformPoint(M3));
         }
         if (transform.TransformPoint(M4).y < 0)
         {
-            body.AddForceAtPosition(new Vector3(0,(d4*d4*(d1+d2)*mass),0), transform.TransformPoint(M4));
+            body.AddForceAtPosition(new Vector3(0,(d4*d4*(d1+d2)*transform.TransformPoint(M4).y*mass*drag),0), transform.TransformPoint(M4));
         }*/
         
         if (transform.TransformPoint(M1).y < 0)
         {
-            body.AddForceAtPosition(new Vector3(0,Math.Abs(transform.TransformPoint(M1).y*mass*drag),0), transform.TransformPoint(M1));
+            body.AddForceAtPosition(new Vector3(0,Math.Abs(transform.TransformPoint(M1).y*mass*drag*(float)Math.Sqrt(d1)),0), transform.TransformPoint(M1));
         }
         if (transform.TransformPoint(M2).y < 0)
         {
-            body.AddForceAtPosition(new Vector3(0,Math.Abs(transform.TransformPoint(M2).y*mass*drag),0), transform.TransformPoint(M2));
+            body.AddForceAtPosition(new Vector3(0,Math.Abs(transform.TransformPoint(M2).y*mass*drag*(float)Math.Sqrt(d2)),0), transform.TransformPoint(M2));
         }
         if (transform.TransformPoint(M3).y < 0)
         {
-            body.AddForceAtPosition(new Vector3(0,Math.Abs(transform.TransformPoint(M3).y*mass*drag),0), transform.TransformPoint(M3));
+            body.AddForceAtPosition(new Vector3(0,Math.Abs(transform.TransformPoint(M3).y*mass*drag*(float)Math.Sqrt(d3)),0), transform.TransformPoint(M3));
         }
         if (transform.TransformPoint(M4).y < 0)
         {
-            body.AddForceAtPosition(new Vector3(0,Math.Abs(transform.TransformPoint(M4).y*mass*drag),0), transform.TransformPoint(M4));
+            body.AddForceAtPosition(new Vector3(0,Math.Abs(transform.TransformPoint(M4).y*mass*drag*(float)Math.Sqrt(d4)),0), transform.TransformPoint(M4));
         }
     }
 
